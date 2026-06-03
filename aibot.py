9dda8c9bdedeb2ae43e594241adb7d725323b643
renaroundtree-sudo <rena.roundtree@gmail.com>
2026-06-03 16:08:28 -0400

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -5,7 +5,7 @@ import requests
 import json
 
 def response_generator():
-    response = ai_ask("Pretend you are a very friendly and helpful person.  Please provide a response given the provided context.  Please provide the response only with no before or after commentary.",
+    response = ai_ask("Pretend you are an educated black woman.  Please provide a response given the provided context.  Please provide the response only with no before or after commentary.",
                       data=st.session_state.messages,
                       api_key=st.secrets["apikey"])
     for word in response.split():
